e08dec54c98347779c37cf3c812774bbaf847d59
Floyd Hightower <floyd.hightower27@gmail.com>
2021-04-28 19:20:05 -0500

    Updating demo data

--- demo/cal/management/commands/loaddemo.py
+++ demo/cal/management/commands/loaddemo.py
@@ -4,6 +4,7 @@
 | and the database will be pre-populated with some data relative to today's date. |
 #---------------------------------------------------------------------------------+
 '''
+import random
 import os
 import django
 from django.core.management import call_command
@@ -24,90 +25,47 @@ class Term:
 
 
 def create_sample_data():
-    
     # Create the studio's event types
     ets = dict((
         (abbr, swingtime.EventType.objects.create(abbr=abbr, label=label))
         for abbr, label in (
-            ('prv',  'Private Lesson'),
-            ('bgn',  'Beginner Class'),
-            ('adv',  'Advanced Class'),
-            ('bbc',  'Black Belt Class'),
-            ('spr',  'Sparring'),
-            ('open', 'Open Dojo'),
-            ('spc',  'Special Event'),
+            ('cs',  'C-Section'),
+            ('ind',  'Induction'),
         )
     ))
     print(__doc__)
     print('Created event types: %s' % (
         ', '.join(['%s' % et for et in swingtime.EventType.objects.all()]),
     ))
-    
     now = datetime.now()
-    
+
     # create a single occurrence event
     evt = swingtime.create_event(
-        'Grand Opening',
-        ets['spc'],
-        description='Open house',
+        'C-Section',
+        ets['cs'],
+        description='C-Section',
         start_time=datetime.combine(now.date(), time(16)),
         end_time=datetime.combine(now.date(), time(18)),
-        note='Free tea, sushi, and sake'
-    )
-    print('Created event "%s" with %d occurrences' % (evt, evt.occurrence_set.count()))
-    
-    # create an event with multiple occurrences by fixed count
-    evt = swingtime.create_event(
-        'Beginner Class',
-        ets['bgn'],
-        description='Open to all white and yellow belts',
-        start_time=datetime.combine(now.date(), time(19)),
-        count=30,
-        byweekday=(rrule.MO, rrule.WE, rrule.FR)
+        note='C-Section for the Fair Weather OB Office'
     )
     print('Created event "%s" with %d occurrences' % (evt, evt.occurrence_set.count()))
 
-    # create an event with multiple occurrences by ending date (until)
-    evt = swingtime.create_event(
-        'Advance Class',
-        ets['adv'],
-        description='Open to all green and brown belts',
-        start_time=datetime.combine(now.date(), time(18)),
-        until=now + timedelta(days=+70),
-        byweekday=(rrule.MO, rrule.WE, rrule.FR)
-    )
-    print('Created event "%s" with %d occurrences' % (evt, evt.occurrence_set.count()))
-
-    # create an event with multiple occurrences by fixed count on monthly basis
-    evt = swingtime.create_event(
-        'Black Belt Class',
-        ets['bbc'],
-        description='Open to all black belts',
-        start_time=datetime.combine(now.date(), time(18, 30)),
-        end_time=datetime.combine(now.date(), time(20, 30)),
-        count=6,
-        freq=rrule.MONTHLY,
-        byweekday=(rrule.TH(+1), rrule.TH(+3))
-    )
-    print('Created event "%s" with %d occurrences' % (evt, evt.occurrence_set.count()))
-
-    # create an event with multiple occurrences and alternate intervale
-    evt = swingtime.create_event(
-        'Open Dojo',
-        ets['open'],
-        description='Open to all students',
-        start_time=datetime.combine(now.date(), time(12)),
-        end_time=datetime.combine(now.date(), time(16)),
-        interval=2,
-        count=6,
-        byweekday=(rrule.SU)
-    )
-    print('Created event "%s" with %d occurrences\n' % (evt, evt.occurrence_set.count()))
+    for i in range(10):
+        # create an event with multiple occurrences by fixed count
+        evt = swingtime.create_event(
+            'Induction',
+            ets['ind'],
+            description='Induction',
+            start_time=datetime.combine(now.date(), time(random.randint(9, 19))),
+            count=2,
+            byweekday=(rrule.MO, rrule.WE, rrule.FR),
+            note="Induction for the Fair Ponds OB Office"
+        )
+        print('Created event "%s" with %d occurrences' % (evt, evt.occurrence_set.count()))
 
 
 class Command(BaseCommand):
     help = 'Run the swingtime demo. If an existing demo database exists, it will recreated.'
-    
     def handle(self, **options):
         dbpath = settings.DATABASES['default']['NAME']
         if os.path.exists(dbpath):
