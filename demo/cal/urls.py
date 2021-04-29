fa5af3a881f969fbc10e6822ab7be8ce0693475e
Floyd Hightower <floyd.hightower27@gmail.com>
2021-04-28 19:03:39 -0500

    Simplifying urls and views (and adding simple login demo)

--- demo/cal/urls.py
+++ demo/cal/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
-from cal import views
-
 urlpatterns = [
-    url(r'^', include('swingtime.urls')),
-    url(r'^events/type/([^/]+)/$', views.event_type, name='karate-event'),
+    url(r'^', include('swingtime.urls'))
 ]
--- demo/urls.py
+++ demo/urls.py
@@ -3,8 +3,9 @@ from django.views.static import serve
 from django.conf.urls import include, url
 from django.views.generic import TemplateView, RedirectView
 
+from views import login
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/cal/')),
+    url(r'^$', login),
     url(r'^cal/', include('cal.urls')),
 ]
